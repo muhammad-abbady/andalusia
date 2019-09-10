a01cfda6246680be2391e6ed865f8417cfe38191
Omar Tawfik <OmarTawfik@users.noreply.github.com>
2019-09-09 20:24:50 -0700

    Added more linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,12 @@
 Licensed under the MIT License. See LICENSE file in the project root for license information.
 **********************************************************************************************/
 
+const path = require("path");
+
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: path.join(__dirname, "tsconfig.json"),
     ecmaVersion: 2018,
     sourceType: "module",
     ecmaFeatures: {
@@ -21,9 +24,12 @@ module.exports = {
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
+    "plugin:promise/recommended",
     "prettier/@typescript-eslint",
   ],
   rules: {
+    "no-unused-expressions": "error",
+    "@typescript-eslint/no-floating-promises": "error",
     "@typescript-eslint/explicit-function-return-type": [
       "error",
       {
